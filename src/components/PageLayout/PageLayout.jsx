4b7cf3848c666f32846c4c068ecc700281a86e1e
Edgardo Lamase <edgardolamas2000@gmail.com>
2026-02-06 22:54:10 -0300

    fix: layout del disclaimer banner corregido
    - Reestructurado PageLayout con wrapper exterior
    - Banner ahora está arriba del contenedor flex
    - Layout horizontal preservado (sidebar + contenido)

--- src/components/PageLayout/PageLayout.jsx
+++ src/components/PageLayout/PageLayout.jsx
@@ -4,14 +4,16 @@ import './PageLayout.css'
 
 function PageLayout({ children }) {
     return (
-        <div className="page-layout">
+        <div className="page-wrapper">
             <DisclaimerBanner />
-            <Navigation />
-            <main className="page-layout__content">
-                <div className="page-layout__inner">
-                    {children}
-                </div>
-            </main>
+            <div className="page-layout">
+                <Navigation />
+                <main className="page-layout__content">
+                    <div className="page-layout__inner">
+                        {children}
+                    </div>
+                </main>
+            </div>
         </div>
     )
 }
